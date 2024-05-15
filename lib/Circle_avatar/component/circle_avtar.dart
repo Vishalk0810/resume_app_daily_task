7fb9e2dc64a851db58ce1687945aa789c0b6d2e0
Vishal Keshri <vishalkeshri858@gmail.com>
2024-05-15 16:34:00 +0530

    15-05-2024

---
+++ lib/Circle_avatar/component/circle_avtar.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Picker extends StatefulWidget {
+  const Picker({super.key});
+
+  @override
+  State<Picker> createState() => _PickerState();
+}
+
+ImagePicker imagePicker = ImagePicker();
+File? fileImage;
+
+class _PickerState extends State<Picker> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarColor: Colors.white),
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
+        title: Text('Image Picker'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              height: 350,
+              width: 340,
+              decoration: BoxDecoration(
+                  color: Colors.cyan,
+                  image: (fileImage != null)
+                      ? DecorationImage(
+                    fit: BoxFit.cover,
+                          image: FileImage(fileImage!),
+                        )
+                      : null),
+            ),
+          ),
+          SizedBox(
+            height: 70,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  onPressed: () async {
+                    XFile? xFileImage =
+                        await imagePicker.pickImage(source: ImageSource.camera);
+                    setState(() {
+                      fileImage = File(xFileImage!.path);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.camera_alt,
+                    size: 50,
+                  )),
+              IconButton(
+                  onPressed: () async {
+                    XFile? xFileImage = await imagePicker.pickImage(
+                        source: ImageSource.gallery);
+                    setState(() {
+                      fileImage = File(xFileImage!.path);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.image,
+                    size: 50,
+                  ))
+            ],
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            height: 50,
+            width: 360,
+            decoration: BoxDecoration(
+              color: Colors.cyan,
+            ),
+            child: Center(
+              child: Text(
+                'Uplode',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'Circle_avatar/component/circle_avtar.dart';
 import 'gmail_login_page/component/Login_screen.dart';
 import 'gmail_login_page/component/login_password.dart';
 import 'gmail_login_page/successful_page/component/successful_screen.dart';
 class MapRoutes{
   static Map<String, Widget Function(BuildContext)>routes = {
-     '/':(context)=>const GoogleEmailScreen(),
-     '/password':(context)=>const PasswordScreen(),
-     '/success':(context)=>const SuccessScreen(),
+      '/':(context)=>const Picker(),
+     // '/':(context)=>const GoogleEmailScreen(),
+     // '/password':(context)=>const PasswordScreen(),
+     // '/success':(context)=>const SuccessScreen(),
     // '/':(context)=>const Gmail(),
     // '/':(context)=>const EntryPage(),
     // '/login':(context)=>const LoginScreen(),
